b8d27584034bd2619dbfff582f995110365536f1
se77ings <se77ings@gmail.com>
2025-03-18 20:48:56 -0300

    Basic Structure for BaseService + Commons

---
+++ src/Shared/Totem.Common.API/Configurations/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Totem.Common.Domain;
+
+namespace Totem.Common.API.Configurations
+{
+	public static class DependencyInjection
+	{
+		public static void RegisterDependency(this IServiceCollection services)
+		{
+			services.AddScoped<INotificador, Notificador>();
+		}
+	}
+}
---
+++ src/Totem.Application/Services/PasswordServices/PasswordService.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Totem.Common.Data;
+using Totem.Common.Domain;
+using Totem.Domain.Aggregates.PasswordAggregate;
+
+namespace Totem.Application.Services.PasswordServices
+{
+	public class PasswordService : BaseService, IPasswordServices
+	{
+
+		public PasswordService(INotificador notificador) : base(notificador)
+		{
+		}
+
+		public async Task AddPasswordAsync(string code)
+		{
+			if (ExecutarValidacao(new PasswordValidations(), new Password(code)){
+
+			}
+		}
+	}
+}
