6317c4af99e805ca8a260b788213b90c5160e297
se77ings <se77ings@gmail.com>
2025-03-25 19:49:13 -0300

    Criação de Labels.

--- src/Totem.Domain/Aggregates/ServiceLocationAggregate/ServiceLocation.cs
+++ src/Totem.Domain/Aggregates/ServiceLocationAggregate/ServiceLocation.cs
@@ -4,13 +4,13 @@
 	{
 		public string Name { get; private set; }
 		public int? Number { get; private set; }
-		
+	
 		public ServiceLocation(string name, int? number = null)
 		{
 			if (string.IsNullOrWhiteSpace(name))
 				//TODO: Implement Resource files (.resx)
-				throw new ArgumentException("O nome do local é obrigatório.", nameof(name)); 
-
+				throw new ArgumentException($"Não foi possível criar o Local de Atendimento", nameof(name)); 
+			
 			Name = name;
 			Number = number;
 		}
