61e09229c02f086163b44419701ed6aa022b0d75
Abner santos <s.abner@aluno.ifsp.edu.br>
2025-03-31 19:36:59 -0300

    Refatoração e melhorias no gerenciamento de senhas
    - Adicionado método `AddNotifications` em INotificador e Notificador.
    - Classes `Errors` e `Messages` tornadas públicas com novos métodos de acesso.
    - Atualização do arquivo `Errors.resx` para a versão 2.0 com novas mensagens de erro.
    - Modificações em `BaseService` para incluir métodos de sucesso e insucesso.
    - Atualização de `IPasswordService` com novos métodos para manipulação de senhas.
    - Implementação da lógica de senhas em `PasswordService`.
    - Ajustes em `IPasswordRepository` e `Password` para novas propriedades.
    - Validações adicionadas em `PasswordRequest`.
    - Remoção de `PasswordQuery` e implementação em `PasswordQueries`.
    - Introdução da classe `Result` para encapsular lógica de sucesso e erro.
    - Adição de `DependencyInjection` para registro de dependências.
    - Atualização de `Program.cs` para incluir registro de dependências.

--- src/Shared/Totem.Common/Services/BaseService.cs
+++ src/Shared/Totem.Common/Services/BaseService.cs
@@ -5,33 +5,85 @@ using Totem.Common.Domain.Notification;
 
 namespace Totem.Common.Services
 {
-	public abstract class BaseService
-	{
-		private readonly INotificador _notificador;
-		protected BaseService(INotificador notificador)
-		{
-			_notificador = notificador;
-		}
-		protected void Notificar(ValidationResult validationResult)
-		{
-			foreach (var item in validationResult.Errors)
-			{
-				Notificar(item.ErrorMessage);
-			}
-		}
+    public abstract class BaseService
+    {
 
-		protected void Notificar(string mensagem)
-		{
-			_notificador.Handle(new Notificacao(mensagem));
-		}
-		protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
-		{
-			var validator = validacao.Validate(entidade);
-			if (validator.IsValid) return true;
+        private readonly INotificador _notificador;
 
-			Notificar(validator);
-			return false;
-		}
-	}
+        protected BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected bool ExecuteValidation<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+            if (validator.IsValid) return true;
+            Notificar(validator);
+            return false;
+        }
+
+        protected Result Successful()
+        {
+            return new Result();
+        }
+
+        protected (Result result, T data) Successful<T>()
+        {
+            return (new Result(), default(T));
+        }
+
+        protected (Result result, T data) Successful<T>(T data)
+        {
+            return (new Result(), data);
+        }
+
+        protected Result Unsuccessful(List<Notificacao> notificacaos)
+        {
+            var result = new Result();
+            result.AddNotifications(notificacaos);
+
+            return result;
+        }
+
+        protected (Result result, T data) Unsuccessful<T>()
+        {
+            return (Unsuccessful(), default(T));
+        }
+        protected (Result result, T data) Unsuccessful<T>(string message)
+        {
+            return (Unsuccessful(message), default(T));
+        }
+
+        protected Result Unsuccessful(string message)
+        {
+            var result = new Result();
+            result.Handle(new Notificacao(message));
+            return result;
+        }
+
+
+        protected Result Unsuccessful()
+        {
+            var result = new Result();
+            result.ObterNotificacoes();
+
+            return result;
+        }
+
+    }
 
 }
