c2bd86a97a4e3ae72b66291d2e9d256ca7b23e10
Abner santos <s.abner@aluno.ifsp.edu.br>
2025-04-27 16:13:57 -0300

    Corrigido o evento que nao queria ser acionado, era a falta de injeção no container de dependencia.

---
+++ src/Totem.Domain/Aggregates/PasswordAggregate/Events/AssignNextPasswordRequestedEvent.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace Totem.Domain.Aggregates.PasswordAggregate.Events
+{
+    public record AssignNextPasswordRequestedEvent(Guid QueueId, Guid ServiceLocationId) : INotification;
+}
