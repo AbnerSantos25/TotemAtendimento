e9f871dbb9a024cb267ef6a7843215827069c1a3
se77ings <se77ings@gmail.com>
2025-04-28 19:14:53 -0300

    Back-end para marcar senha como Atendida finalizado.

--- src/Totem.API/Controllers/PasswordController.cs
+++ src/Totem.API/Controllers/PasswordController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 using Totem.Application.Services.PasswordServices;
 using Totem.Common.API.Controller;
 using Totem.Common.Domain.Notification;
@@ -6,54 +7,61 @@ using Totem.Domain.Models.PasswordModels;
 
 namespace Totem.API.Controllers
 {
-    [Route("api/totem/[controller]")]
-    public class PasswordController : MainController
-    {
-        private readonly IPasswordService _passwordService;
-        public PasswordController(INotificador notificador, IPasswordService passwordService) : base(notificador)
-        {
-            _passwordService = passwordService;
-        }
+	[Route("api/totem/[controller]")]
+	public class PasswordController : MainController
+	{
+		private readonly IPasswordService _passwordService;
+		public PasswordController(INotificador notificador, IPasswordService passwordService) : base(notificador)
+		{
+			_passwordService = passwordService;
+		}
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetPasswordById(Guid id) {
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetPasswordById(Guid id)
+		{
 
-            if(!ModelState.IsValid) return CustomResponse(ModelState);
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-            return CustomResponse(await _passwordService.GetByIdPasswordAsync(id));
-        }
-
-
-        [HttpGet]
-        public async Task<IActionResult> GetPasswords()
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-
-            return CustomResponse(await _passwordService.GetListPasswordAsync());
-        }
+			return CustomResponse(await _passwordService.GetByIdPasswordAsync(id));
+		}
 
 
-        [HttpPost]
-        public async Task<IActionResult> AddPassword([FromBody] PasswordRequest request)
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-            var teste = await _passwordService.AddPasswordAsync(request);
-            return CustomResponse(teste);
-        }
+		[HttpGet]
+		public async Task<IActionResult> GetPasswords()
+		{
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
 
-        [HttpPost("transfer")]
-        public async Task<IActionResult> Transfer([FromBody] TransferPasswordRequest request)
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
+			return CustomResponse(await _passwordService.GetListPasswordAsync());
+		}
 
-            return CustomResponse(await _passwordService.TransferPasswordAsync(request.PasswordId,request.NewQueueId));
-        }
 
-        [HttpDelete]
-        public async Task<IActionResult> RemovePassword(Guid id)
-        {
-            if (!ModelState.IsValid) return CustomResponse(ModelState);
-            return CustomResponse(await _passwordService.RemovePasswordAsync(id));
-        }
-    }
+		[HttpPost]
+		public async Task<IActionResult> AddPassword([FromBody] PasswordRequest request)
+		{
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
+			var teste = await _passwordService.AddPasswordAsync(request);
+			return CustomResponse(teste);
+		}
+
+		[HttpPost("transfer")]
+		public async Task<IActionResult> Transfer([FromBody] TransferPasswordRequest request)
+		{
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+			return CustomResponse(await _passwordService.TransferPasswordAsync(request.PasswordId, request.NewQueueId));
+		}
+
+		[HttpPatch("{passwordId}/MarkAsServed")]
+		public async Task<IActionResult> MarkAsServedAsync([FromRoute] Guid passwordId)
+		{
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
+			return CustomResponse(await _passwordService.MarkAsServed(passwordId));
+		}
+		[HttpDelete]
+		public async Task<IActionResult> RemovePassword(Guid id)
+		{
+			if (!ModelState.IsValid) return CustomResponse(ModelState);
+			return CustomResponse(await _passwordService.RemovePasswordAsync(id));
+		}
+	}
 }
--- src/Totem.Application/Services/PasswordServices/PasswordService.cs
+++ src/Totem.Application/Services/PasswordServices/PasswordService.cs
@@ -11,114 +11,131 @@ using Totem.Domain.Models.PasswordModels;
 
 namespace Totem.Application.Services.PasswordServices
 {
-    public class PasswordService : BaseService, IPasswordService
+	public class PasswordService : BaseService, IPasswordService
 	{
-        private readonly IPasswordRepository _passwordRepository;
-        private readonly IPasswordQueries _passwordQueries;
-        private readonly IMediator _mediator;
-        private readonly IRealTimeNotifier _notifier;
+		private readonly IPasswordRepository _passwordRepository;
+		private readonly IPasswordQueries _passwordQueries;
+		private readonly IMediator _mediator;
+		private readonly IRealTimeNotifier _notifier;
 
-        public PasswordService(INotificador notificador, IPasswordRepository passwordRepository, IPasswordQueries passwordQueries, PasswordValidations passwordValidation, IServiceLocationRepository serviceLocationRepository, IMediator mediator, IRealTimeNotifier notifier) : base(notificador)
-        {
-            _passwordRepository = passwordRepository;
-            _passwordQueries = passwordQueries;
-            _mediator = mediator;
-            _notifier = notifier;
-        }
-        public async Task<(Result result, Guid data)> AddPasswordAsync(PasswordRequest request)
-        {
-            var password = new Password(request.QueueId,request.Preferential);
-            if (!ExecuteValidation(new PasswordValidations(), password))
-                return Unsuccessful<Guid>();
+		public PasswordService(INotificador notificador, IPasswordRepository passwordRepository, IPasswordQueries passwordQueries, PasswordValidations passwordValidation, IServiceLocationRepository serviceLocationRepository, IMediator mediator, IRealTimeNotifier notifier) : base(notificador)
+		{
+			_passwordRepository = passwordRepository;
+			_passwordQueries = passwordQueries;
+			_mediator = mediator;
+			_notifier = notifier;
+		}
+		public async Task<(Result result, Guid data)> AddPasswordAsync(PasswordRequest request)
+		{
+			var password = new Password(request.QueueId, request.Preferential);
+			if (!ExecuteValidation(new PasswordValidations(), password))
+				return Unsuccessful<Guid>();
 
-            password.IncrementCode(await _passwordRepository.GetNextPasswordCodeAsync());
-            
-            _passwordRepository.Add(password);
+			password.IncrementCode(await _passwordRepository.GetNextPasswordCodeAsync());
 
-            try
-            {
-                if (!await _passwordRepository.UnitOfWork.CommitAsync())
-                    return Unsuccessful<Guid>(Errors.ErrorSavingDatabase.ToString());
-            }
-            catch (Exception ex)
-            {
+			_passwordRepository.Add(password);
 
-            }
+			try
+			{
+				if (!await _passwordRepository.UnitOfWork.CommitAsync())
+					return Unsuccessful<Guid>(Errors.ErrorSavingDatabase.ToString());
+			}
+			catch (Exception ex)
+			{
 
-            await _mediator.Publish(new PasswordCreatedEvent(password.Id, password.QueueId));
+			}
 
-            return Successful(password.Id);
-        }
+			await _mediator.Publish(new PasswordCreatedEvent(password.Id, password.QueueId));
 
-
-        public async Task<Result> TransferPasswordAsync(Guid passwordId, Guid queueId)
-        {
-            var password = await _passwordRepository.GetByIdAsync(passwordId);
-            if (password == null)
-                return Unsuccessful(Errors.PasswordNotFound.ToString());
-
-            password.AssignToQueue(queueId);
-
-            _passwordRepository.Update(password);
-
-            if (!await _passwordRepository.UnitOfWork.CommitAsync())
-                return Unsuccessful(Errors.ErrorSavingDatabase.ToString());
-
-            await _mediator.Publish(new PasswordCreatedEvent(passwordId, queueId));
-
-            return Successful();
-        }
-
-        public async Task<(Result result, PasswordView data)> AssignNextPasswordAsync(Guid queueId, Guid serviceLocationId)
-        {
-            var nextPassword = await _passwordRepository
-                .GetNextUnassignedPasswordFromQueueAsync(queueId);
-
-            if (nextPassword == null)
-                return Successful<PasswordView>(null); // Não há senhas disponíveis na fila
-
-            nextPassword.AssignToServiceLocation(serviceLocationId);
-
-            _passwordRepository.Update(nextPassword);
-
-            if (!await _passwordRepository.UnitOfWork.CommitAsync())
-                return Unsuccessful<PasswordView>(Errors.ErrorSavingDatabase.ToString());
-            
-            await _mediator.Publish(new ServiceLocationReadyEvent(serviceLocationId, queueId));
-
-            return Successful(nextPassword);
-        }
-
-        public async Task<(Result result, PasswordView data)> GetByIdPasswordAsync(Guid id)
-        {
-            var password = await _passwordRepository.GetByIdAsync(id);
-            if (password == null)
-                return Unsuccessful<PasswordView>(Errors.PasswordNotFound.ToString());
-                
-
-            return Successful(password);
-        }
-
-        public async Task<(Result result, List<PasswordView> data)> GetListPasswordAsync()
-        {
-            var passwords = await _passwordQueries.GetListPasswordsAsync();
-            return Successful(passwords);
-        }
-
-        public async Task<Result> RemovePasswordAsync(Guid id)
-        {
-            var password = await _passwordRepository.GetByIdAsync(id);
-            if (password == null)
-                return Unsuccessful(Errors.PasswordNotFound.ToString());
-
-            _passwordRepository.Delete(password);
-
-            if (!await _passwordRepository.UnitOfWork.CommitAsync())
-                return Unsuccessful(Errors.ErrorSavingDatabase.ToString());
-
-            return Successful();
-        }
+			return Successful(password.Id);
+		}
 
 
-    }
+		public async Task<Result> TransferPasswordAsync(Guid passwordId, Guid queueId)
+		{
+			var password = await _passwordRepository.GetByIdAsync(passwordId);
+			if (password == null)
+				return Unsuccessful(Errors.PasswordNotFound.ToString());
+
+			password.AssignToQueue(queueId);
+
+			_passwordRepository.Update(password);
+
+			if (!await _passwordRepository.UnitOfWork.CommitAsync())
+				return Unsuccessful(Errors.ErrorSavingDatabase.ToString());
+
+			await _mediator.Publish(new PasswordCreatedEvent(passwordId, queueId));
+
+			return Successful();
+		}
+
+		public async Task<(Result result, PasswordView data)> AssignNextPasswordAsync(Guid queueId, Guid serviceLocationId)
+		{
+			var nextPassword = await _passwordRepository
+				.GetNextUnassignedPasswordFromQueueAsync(queueId);
+
+			if (nextPassword == null)
+				return Successful<PasswordView>(null); // Não há senhas disponíveis na fila
+
+			nextPassword.AssignToServiceLocation(serviceLocationId);
+
+			_passwordRepository.Update(nextPassword);
+
+			if (!await _passwordRepository.UnitOfWork.CommitAsync())
+				return Unsuccessful<PasswordView>(Errors.ErrorSavingDatabase.ToString());
+
+			await _mediator.Publish(new ServiceLocationReadyEvent(serviceLocationId, queueId));
+
+			return Successful(nextPassword);
+		}
+
+		public async Task<(Result result, PasswordView data)> GetByIdPasswordAsync(Guid id)
+		{
+			var password = await _passwordRepository.GetByIdAsync(id);
+			if (password == null)
+				return Unsuccessful<PasswordView>(Errors.PasswordNotFound.ToString());
+
+
+			return Successful(password);
+		}
+
+		public async Task<(Result result, List<PasswordView> data)> GetListPasswordAsync()
+		{
+			var passwords = await _passwordQueries.GetListPasswordsAsync();
+			return Successful(passwords);
+		}
+
+		public async Task<Result> RemovePasswordAsync(Guid id)
+		{
+			var password = await _passwordRepository.GetByIdAsync(id);
+			if (password == null)
+				return Unsuccessful(Errors.PasswordNotFound.ToString());
+
+			_passwordRepository.Delete(password);
+
+			if (!await _passwordRepository.UnitOfWork.CommitAsync())
+				return Unsuccessful(Errors.ErrorSavingDatabase.ToString());
+
+			return Successful();
+		}
+
+		public async Task<Result> MarkAsServed(Guid passwordId)
+		{
+			var password = await _passwordRepository.GetByIdAsync(passwordId);
+			if (password == null)
+				return Unsuccessful(Errors.PasswordNotFound.ToString());
+
+			if (password.ServiceLocation == null)
+				return Unsuccessful(Errors.PasswordCannotBeServed.ToString());
+
+			password.MarkAsServed();
+
+			_passwordRepository.Update(password);
+
+			if (!await _passwordRepository.UnitOfWork.CommitAsync())
+				return Unsuccessful(Errors.ErrorSavingDatabase.ToString());
+
+			return Successful();
+		}
+	}
 }
