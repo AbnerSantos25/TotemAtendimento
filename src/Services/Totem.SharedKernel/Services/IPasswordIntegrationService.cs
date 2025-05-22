95cbf6c078a2622a56750d812b1b66f95caf585f
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2025-05-22 20:08:23 -0300

    Adiciona novos projetos e refatora serviços de senha (#46)
    Foram adicionados os projetos `Totem.SharedKernel` e `Totem.SharedIntegrations` ao arquivo de solução `TotemAtendimento.sln`.
    
    O arquivo `BaseService.cs` agora inclui o método `Unsuccessful<T>(List<Notificacao> notificacaos)`. O manipulador de eventos `AssignNextPasswordEventHandler` foi removido, e o controlador `ServiceLocationController.cs` foi atualizado para usar `ServiceLocationReadyAsync`.
    
    Além disso, a interface `IPasswordService` foi alterada para refletir a nova assinatura do método `ServiceLocationWaitingPasswordAsync`, e o arquivo `ServiceLocationService.cs` foi modificado para integrar a lógica de espera de senha.
    
    Novos arquivos foram criados para definir eventos e interfaces relacionadas ao sistema de integração de senhas, incluindo `ServiceLocationWaitingPasswordEvent.cs` e `IPasswordView`.

---
+++ src/Services/Totem.SharedKernel/Services/IPasswordIntegrationService.cs
@@ -0,0 +1,9 @@
+﻿using Totem.SharedKernel.Models;
+
+namespace Totem.SharedKernel.Services
+{
+	public interface IPasswordIntegrationService
+	{
+		Task<( Result result, IPasswordView data)> ServiceLocationWaitingPasswordAsync(Guid queueId, Guid serviceLocationId, string newServiceLocationName);
+	}
+}
