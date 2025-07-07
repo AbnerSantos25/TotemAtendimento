85ccfc1401f2ca17f92f747fe77e686c9e063a92
Abner santos <s.abner@aluno.ifsp.edu.br>
2025-07-07 19:44:55 -0300

    cirando stack

---
+++ src/Mobile/app/features/configurations/_layout.tsx
@@ -0,0 +1,9 @@
+// features/configurations/_layout.tsx
+import { Stack } from 'expo-router'; // Ou Tabs, Drawer, etc. dependendo do seu tipo de navegação
+
+export default function ConfigurationsLayout() {
+  return (
+    <>
+    </>
+  );
+}
---
+++ src/Mobile/app/features/configurations/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+
+
+export default function ConfigurationsScreen() {
+    return(
+        <>
+        <Text>teste </Text>
+        </>
+    );
+}
