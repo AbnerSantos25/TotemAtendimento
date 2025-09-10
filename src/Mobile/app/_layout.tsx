17e8af54798b9c48f5b05218a9ed7ecc76fe877a
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2025-09-09 21:24:37 -0300

    Dev/abner/configuration (#62)
    * cirando stack
    
    * ajustando telas
    
    Teste

--- src/Mobile/app/_layout.tsx
+++ src/Mobile/app/_layout.tsx
@@ -1,20 +1,42 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from 'expo-router';
+import { Pressable } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
+// Este arquivo define a navegação em pilha para o seu aplicativo.
 export default function RootLayout() {
+  const router = useRouter();
+
   return (
     <Stack screenOptions={{
         headerStyle: {
-          backgroundColor: "#222222",
+          backgroundColor: '#121212',
         },
-        headerTintColor: "#fff",
+        headerTintColor: '#fff',
         headerTitleStyle: {
-          fontWeight: "bold",
+          fontWeight: 'bold',
         },
-      }}>
-        
-      <Stack.Screen name="index" options={{ title: "Teste", headerTitleAlign:"center" }} />
-      <Stack.Screen name="features/login" options={{ title: "Login", headerTitleAlign:"center" }} />
-      <Stack.Screen name="features/configurations" options={{ title: "Configurações", headerTitleAlign:"center" }} />
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Início',
+          headerRight: () => (
+            <Pressable onPress={() => router.push('/configuration/configuracoes')}>
+              <FontAwesome 
+                name="cog" 
+                size={25} 
+                color="lightgray" 
+                style={{ marginRight: 15 }}
+              />
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen name="atendimento" options={{ title: 'Atendimento' }} />
+      <Stack.Screen name="preferencial" options={{ title: 'Preferencial' }} />
+      <Stack.Screen name="retirada-exames" options={{ title: 'Retirada de Exames' }} />
+      <Stack.Screen name="configuration/configuracoes" options={{ title: 'Configurações', presentation: 'modal' }}/>
     </Stack>
   );
 }
--- src/Mobile/app/features/configurations/_layout.tsx
+++
@@ -1,9 +0,0 @@
-// features/configurations/_layout.tsx
-import { Stack } from 'expo-router'; // Ou Tabs, Drawer, etc. dependendo do seu tipo de navegação
-
-export default function ConfigurationsLayout() {
-  return (
-    <>
-    </>
-  );
-}
