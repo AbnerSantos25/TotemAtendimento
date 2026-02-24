aa7b37257a3d4dc840355ffdc1c2a3276f061cfd
Abner santos <s.abner@aluno.ifsp.edu.br>
2026-02-24 19:03:34 -0300

    Corrigindo mapeamento e utilização do HexColor

--- src/Services/Totem.Domain/Aggregates/ServiceTypeAggregate/ServiceType.cs
+++ src/Services/Totem.Domain/Aggregates/ServiceTypeAggregate/ServiceType.cs
@@ -3,49 +3,37 @@ using Totem.Common.Domain.Entity;
 
 namespace Totem.Domain.Aggregates.ServiceTypeAggregate
 {
-	public record HexColor
-	{
-		public string Value { get; }
-
-		public HexColor(string value)
-		{
-			if (string.IsNullOrWhiteSpace(value) || !value.StartsWith("#") || value.Length > 7)
-				throw new ArgumentException("Cor inválida.");
-
-			Value = value;
-		}
-
-		public override string ToString()
-		{
-			return Value;
-		}
-	}
-
 	public class ServiceType : Entity, IAggregateRoot
 	{
 		public string Title { get; private set; }
 		public string? Icon { get; private set; }
-		public HexColor Color { get; private set; }
+		public HexColor? Color { get; private set; }
 		public string TicketPrefix { get; private set; }
 		public Guid TargetQueueId { get; private set; }
 		public bool IsActive { get; private set; }
 
 		protected ServiceType(){}
 
-		public ServiceType(string title, string? icon, string color, string ticketPrefix, Guid targetQueueId)
+		public ServiceType(string title, string? icon, string? color, string ticketPrefix, Guid targetQueueId)
 		{
 			Title = title;
 			Icon = icon;
-			Color = new HexColor(color);
+			Color = CreateColor(color);
 			TicketPrefix = ticketPrefix;
 			TargetQueueId = targetQueueId;
 		}
 
-		public void Update(string title, string? icon, string color, Guid targetQueueId)
+		private HexColor? CreateColor(string? color)
+		{
+			if (color is null) return null;
+			return new HexColor(color);
+		}
+
+		public void Update(string title, string? icon, string? color, Guid targetQueueId)
 		{
 			Title = title;
 			Icon = icon;
-			Color = new HexColor(color);
+			Color = CreateColor(color);
 			TargetQueueId = targetQueueId;
 		}
 
---
+++ src/Shared/Totem.Common/ValueObject/HexColor.cs
@@ -0,0 +1,20 @@
+﻿namespace Totem.Domain.Aggregates.ServiceTypeAggregate
+{
+	public record HexColor
+	{
+		public string? Value { get; }
+
+		public HexColor(string value)
+		{
+			if (string.IsNullOrWhiteSpace(value) || !value.StartsWith("#") || value.Length > 7)
+				throw new ArgumentException("Cor inválida.");
+
+			Value = value;
+		}
+
+		public override string ToString()
+		{
+			return Value;
+		}
+	}
+}
