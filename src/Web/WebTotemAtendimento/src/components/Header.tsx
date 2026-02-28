647815239ada9dc5d7a8b6d44006ecf864f1a53a
Abner santos <s.abner@aluno.ifsp.edu.br>
2026-02-28 17:22:22 -0300

    Criando sidebar

--- src/Web/WebTotemAtendimento/src/components/Header.tsx
+++ src/Web/WebTotemAtendimento/src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+
 export function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <h1 className="font-semibold">Painel do Totem</h1>
         </header>
     )
 }
