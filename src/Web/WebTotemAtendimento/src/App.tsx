8c5e48251e532f40d7cc340b6b7d3c70e3bb6d36
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2026-03-03 18:15:11 -0300

    Dev/abner/projeto web (#88)
    * Criação do projeto Vite + React para o Corporate
    
    * Instalação do TWW CSS
    
    * ajustes na estrutura
    
    * Criando sidebar

---
+++ src/Web/WebTotemAtendimento/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Layout } from './components/Layout'
+
+function App() {
+  return (
+    <div className="flex h-screen bg-white dark:bg-zinc-950">
+      <Layout>
+        <h1>App</h1>
+      </Layout>
+    </div>
+  )
+}
+
+export default App
